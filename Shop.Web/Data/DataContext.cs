bbaeaae02e966073d0de5fd09291913437bdbcfc
rayct76 <rayct76@gmail.com>
2020-03-26 13:37:53 -0600

    Add Products Controller.

---
+++ Shop.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Shop.Web.Data
+{
+   
+    using Shop.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
