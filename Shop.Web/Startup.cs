7c4ef9db8334135bd22c24c683407a3d4a93e8d5
rayct76 <rayct76@gmail.com>
2021-02-13 20:05:27 -0600

    ultima version con la api funcionando

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Shop.Web.Data;
 using Shop.Web.Data.Entities;
 using Shop.Web.Helpers;
+using Microsoft.OpenApi.Models;
 
 namespace Shop.Web
 {
@@ -23,6 +24,12 @@ namespace Shop.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
             services.AddIdentity<User, IdentityRole>(cfg =>
             {
                 cfg.User.RequireUniqueEmail = true;
@@ -62,6 +69,17 @@ namespace Shop.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseAuthentication();
